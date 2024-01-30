b042316693e6df95ba0ba3b0b1401b9394ef3b17
Sanjayraj2003 <113586066+Sanjayraj2003@users.noreply.github.com>
2024-01-30 21:42:42 +0530

    Add files via upload

---
+++ BusdataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Bus.Busdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusdataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
